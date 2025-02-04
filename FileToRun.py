a3069a3a44da31e8636e3ab53111f2492827263b
njbca <ncampion@protonmail.com>
2025-02-04 15:36:59 +0100

    Small changes

--- FileToRun.py
+++ FileToRun.py
@@ -3,17 +3,17 @@ import sys
 import os
 
 #---------------User defined----------------
-location = "Utrecht" # Site name for the analysis (must be existing in the "measured data folder")
+location = "Almeria" # Site name for the analysis (must be existing in the "measured data folder")
 years = "All years" #Select a specific year for the analysis (otherwise all years in the measured folder will be used)
 H2_end_user_min_load = "0" # Hydrogen end-user flexibility for the techno-economic assessment (minimal load between 0 and 1)
 solver = "GUROBI_CMD" # Or HiGHS. To use other solvers modify the file '3-Techno-eco_assessment.py'
 renewablesninja_token = "your_token_here" # Replace with your Renewables.ninja token to be able to use their API, e.g. "12345678910"
 
 # List of scripts to run (in order)
-scripts = ["1-PV_simulation_download.py"] 
+#scripts = ["1-PV_simulation_download.py"] 
 #scripts = ["2-PV_Timeseries_analysis.py"] 
 #scripts = ["2-PV_Timeseries_analysis.py", "3-H2_Technoeco_assessment.py"]
-#scripts = ["1-PV_simulation_download.py", "2-PV_Timeseries_analysis.py", "3-H2_Technoeco_assessment.py"]
+scripts = ["1-PV_simulation_download.py", "2-PV_Timeseries_analysis.py", "3-H2_Technoeco_assessment.py"]
 
 #-------------------------------------------
 
